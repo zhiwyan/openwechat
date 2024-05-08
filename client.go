42f784a84ee900c74f01c9f70525230cc1a40fbe
zhiwyan <18612334831@163.com>
2024-05-08 17:23:46 +0800

    Update client.go

--- client.go
+++ client.go
@@ -660,8 +660,8 @@ func (c *Client) WebWxSendAppMsg(ctx context.Context, msg *SendMessage, request 
 
 // WebWxSendAppMsgUrl 发送app消息
 func (c *Client) WebWxSendAppMsgUrl(ctx context.Context, opt *ClientWebWxSendMsgOptions) (*http.Response, error) {
-	opt.Message.Type = MsgTypeApp
-	path, err := url.Parse(c.Domain.BaseHost() + webwxsendmsg)
+	opt.Message.Type = AppMsgTypeUrl
+	path, err := url.Parse(c.Domain.BaseHost() + webwxsendappmsg)
 	if err != nil {
 		return nil, err
 	}
